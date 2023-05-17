5d73658206e3bc3873b3342421a126cafa714d97
arerimr <raydelys.morrobelr@gmail.com>
2023-05-17 19:27:46 -0400

    finisn index page

--- src/Components/Cacti.js
+++ src/Components/Cacti.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react';
+import Cactus from './Cactus';
+
+const API = process.env.REACT_APP_API_URL;
 
 export default function Cacti() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [cacti, setCacti] = useState([])
+
+    useEffect(() => {
+        axios.get(`${API}/cacti`)
+            .then((res) => setCacti(res.data))
+            .catch((e) => console.warn(e))
+    }, [])
+
+    return (
+        <div>
+            {
+                cacti.map((cactus) => {
+                    return <Cactus key={cactus.id} cactus={cactus} />
+                })
+            }
+        </div>
+    )
 }
