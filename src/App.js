1c754ec85eb0b1a8c1e2fa92d07dd5e5e6fac5ef
arerimr <raydelys.morrobelr@gmail.com>
2023-05-17 19:14:37 -0400

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Cacti from './Components/Cacti';
+import CactusDetails from './Components/CactusDetails';
+import NewCactus from './Components/NewCactus';
+import NotFound from './Components/NotFound';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cacti' element={<Cacti />} />
+        <Route path='/cacti/:id' element={<CactusDetails />} />
+        <Route path='/new' element={<NewCactus />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
