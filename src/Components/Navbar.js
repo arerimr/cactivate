31f1b488f447444817a3259763c3f5b02b1a6002
arerimr <raydelys.morrobelr@gmail.com>
2023-05-17 19:50:41 -0400

    finish navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='navbar'>
+            <h1><Link to='/'>Cac🌵ivate</Link></h1>
+            <h2><Link to='/cacti'>CACTI LIST</Link></h2>
+            <button><h2><Link to='/new'>ADD NEW CACTUS</Link></h2></button>
+        </div>
+    )
 }
