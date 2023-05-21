d2b60add7ede5fd1b39309f3d4549ee28e809bcd
arerimr <raydelys.morrobelr@gmail.com>
2023-05-21 10:13:02 -0400

    add to favorite feature

--- src/Components/Cactus.js
+++ src/Components/Cactus.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Cactus({ cactus }) {
+    const [isFavorite, setFavorite] = useState(false)
+
+    const handleFavorite = () => {
+        setFavorite(!isFavorite)
+    }
+
     return (
         <div className='cactus'>
-            <h2>{cactus.name} Cactus</h2><span className='icons'>{cactus.flowers ? "🌸" : null}{cactus.edible ? "🍴" : null}</span>
+            <h2><button onClick={handleFavorite}>{isFavorite ? "❤️" : "♡"}</button> {cactus.name} Cactus</h2>
+            <span className='icons'>{cactus.flowers ? "🌸" : null}{cactus.edible ? "🍴" : null}</span>
             <Link to={`/cacti/${cactus.id}`}><img src={cactus.image} alt={cactus.name} /></Link>
             <h3>{cactus.origin}</h3>
         </div>
