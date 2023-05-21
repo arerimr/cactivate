3c30a93af7de19b7a26b547a7fe07883a6b6ba60
arerimr <raydelys.morrobelr@gmail.com>
2023-05-21 14:27:54 -0400

    enhanced home page

--- src/Components/Puzzle.js
+++ src/Components/Puzzle.js
@@ -3,28 +3,29 @@ import { JigsawPuzzle } from 'react-jigsaw-puzzle/lib'
 import 'react-jigsaw-puzzle/lib/jigsaw-puzzle.css'
 import './puzzle.css'
 import cactus from '../Assets/cactus.jpg'
+import { Link } from 'react-router-dom'
 
 export default function Puzzle() {
     const [text, setText] = useState("Unpuzzle the pieces🌵");
 
     const set = () => {
-        setText("Congratulations!");
+        setText(`Prickly Pear (aka nopal) Cactus!`);
     };
     return (
         <div className='puzzle'>
-            <h2 className="tag">{text}</h2>
+            <h2 className="tag">{text}<Link style={text === "Unpuzzle the pieces🌵" ? { display: "none" } : { display: "block" }} to={`/cacti/1`}>Learn more!</Link></h2>
             <section>
-            <JigsawPuzzle
-                imageSrc={cactus}
-                rows={3}
-                columns={3}
-                onSolved={set}
-                className="jigsaw-puzzle"
-            />
-            <div>
-            <img src={cactus} alt='puzzle solved'/>
-            </div>
-            <br />
+                <JigsawPuzzle
+                    imageSrc={cactus}
+                    rows={3}
+                    columns={3}
+                    onSolved={set}
+                    className="jigsaw-puzzle"
+                />
+                <div>
+                    <img src={cactus} alt='puzzle solved' />
+                </div>
+                <br />
             </section>
         </div>
     )
