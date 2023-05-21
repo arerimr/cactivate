e7bbabcee16e8563f985936da6efdfe9bddba1f4
arerimr <raydelys.morrobelr@gmail.com>
2023-05-20 21:10:30 -0400

    finish footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import { ExternalLink } from "react-external-link"
 
 export default function Footer() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="footer">
+            <div>
+                <h4>Raydelys Morrobel Reyes</h4>
+                <span>Full Stack Web Developer</span>
+                <span>Pursuit Fellow 9.4 N&W</span>
+            </div>
+            <p>Github: <ExternalLink href="https://github.com/arerimr" target="_blank">@arerimr</ExternalLink ></p>
+            <p>Linkedin: <ExternalLink href="https://www.linkedin.com/in/raydelysmr" target="_blank">Raydelys M R</ExternalLink></p>
+        </div>
+    )
 }
